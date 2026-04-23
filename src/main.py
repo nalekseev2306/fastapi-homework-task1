7acff108bb9333046007c8d23325940ae88a1a81
nalekseev <alekseevn2306@gmail.com>
2026-04-23 11:05:50 +0400

    fix docker

--- src/main.py
+++ src/main.py
@@ -4,26 +4,20 @@ import uvicorn
 from app import create_app
 from core.config import settings
 
-# создаём приложение
 app = create_app()
 
 
-# описывает как запустить приложение
-async def run() -> None:
-    config = uvicorn.Config("main:app", host="localhost", port=settings.PORT, reload=False)
-    server = uvicorn.Server(config=config) # создаём сервер по конфигу
-
-    # предоставляем задачи через кортеж
+async def main() -> None:
+    config = uvicorn.Config(
+        "main:app", host="0.0.0.0", port=settings.PORT, reload=False
+    )
+    server = uvicorn.Server(config=config)
     tasks = (
         asyncio.create_task(server.serve()),
     )
 
-    # пока не выполнится хотя бы первая задача
     await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
 
-# точка входа в программу
-if __name__ == "__main__":
 
-    # запускаем цикл ивентов пока не выполнится
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+if __name__ == "__main__":
+    asyncio.run(main())
